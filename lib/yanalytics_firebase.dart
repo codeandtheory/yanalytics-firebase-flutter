8030501f477a23b19462771dec2aec79ddfa0288
Daniel Cardona Rojas <daniel.cardona@ymedialabs.com>
2023-04-19 16:36:30 -0500

    [CM-1367] Repo configuration

---
+++ lib/yanalytics_firebase.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library yanalytics_firebase;
+
+export 'src/yanalytics_firebase_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
